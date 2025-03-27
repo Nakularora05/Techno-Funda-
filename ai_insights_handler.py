961091b6f080fa8e43f16d8e7459346fa626e196
Nakularora05 <141902050+Nakularora05@users.noreply.github.com>
2025-03-27 17:24:30 +0530

    ai_insights_handler.py

--- ai_insights_handler.py
+++ ai_insights_handler.py
@@ -1,17 +1,14 @@
-import requests
+import google.generativeai as genai
+import PIL.Image
 
 class AIInsights:
-    def __init__(self, api_key):
-        self.api_key = api_key
-
-    def get_ai_insights(self, image_path, ticker, market):
-        url = "https://api.example.com/ai-insights"
-        files = {'file': open(image_path, 'rb')}
-        data = {'ticker': ticker, 'market': market}
-        headers = {'Authorization': f'Bearer {self.api_key}'}
-        response = requests.post(url, files=files, data=data, headers=headers)
+    def __init__(self,api_key):
+        self.api_key=api_key
+        genai.configure(api_key=self.api_key)
+        self.model = genai.GenerativeModel(model_name = "gemini-2.0-flash")
         
-        if response.status_code == 200:
-            return response.json()
-        else:
-            raise Exception("Error fetching AI insights")
+    def get_ai_insights(self,image_path,stock,market):
+        image = PIL.Image.open(image_path)
+        prompt = f"This is an image of stock performance of stock : '{stock}' for the last 100 days on market : '{market}', on the basis of volume traded, closing prices and 7,20 day moving averages provide some analysis and suggestion abot this stock. This Stock should be purchased or not."
+        response = self.model.generate_content([prompt, image])
+        return response
